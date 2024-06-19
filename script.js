b458f05305225593eb1f533c07965ea5d9c07804
Dean Ridgwell <deano1592@hotmail.com>
2024-06-19 14:18:00 +0100

    Adding initial files

---
+++ script.js
@@ -0,0 +1,32 @@
+const database = [
+  {
+    username: "Deano",
+    password: "1592",
+  },
+];
+
+const newsFeed = [
+  {
+    user: "Bob",
+    timeline: "I hate work",
+  },
+  {
+    user: "Bill",
+    timeline: "Work is so boring",
+  },
+  {
+    user: "Ben",
+    timeline: "Need a new job",
+  },
+];
+
+const userNamePrompt = prompt("What's your usename?");
+const passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+  if (user === database[0].username && pass === database[0].password) {
+    console.log(newsFeed);
+  } else {
+    alert("Sorry, I don't know you");
+  }
+}
