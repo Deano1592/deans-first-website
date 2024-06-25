f0d641355577b2836dd1769a502860e424491874
Dean Ridgwell <deano1592@hotmail.com>
2024-06-25 13:30:39 +0100

    Adding for loop for database

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const database = [
     username: "Deano",
     password: "1592",
   },
+  {
+    username: "Dillon",
+    password: "1234",
+  },
+  {
+    username: "Darren",
+    password: "9999",
+  },
 ];
 
 const newsFeed = [
@@ -24,7 +32,7 @@ const userNamePrompt = prompt("What's your usename?");
 const passwordPrompt = prompt("What's your password?");
 
 function signIn(user, pass) {
-  if (user === database[0].username && pass === database[0].password) {
+  if (user === database[i].username && pass === database[i].password) {
     console.log(newsFeed);
   } else {
     alert("Sorry, I don't know you");
